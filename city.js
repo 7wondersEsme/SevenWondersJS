120d34fc913f7127ca88a6954a0841537167b5a0
Methacrylon <methacrylon@gmail.com>
2018-05-19 18:22:50 +0200

    Add divinity to city and complete trader test in city

--- city.js
+++ city.js
@@ -1,5 +1,7 @@
 const EventEmitter = require('events');
 const {Soldier} = require('./soldier');
+const {Trader} = require('./trader');
+const {Divinity} = require('./divinity');
 
 class City {
   constructor(name, timeFactor) {
@@ -10,22 +12,69 @@ class City {
     this.timeFactor_ = timeFactor || 1000;
     this.soldiers_ = {};
     this.sId_ = 0;
+    this.divinity = new Divinity('div', this.timeFactor_);
+    this.life = 100000;
   }
 
   init() {
-    this.gaiaInterval_ = setInterval(() => {
-    }, this.timeFactor);
+    this.divinity.init();
+    this.divinity.worldEvents.on('favor', favor => {
+      this.gold_ += favor.gold;
+      this.corn_ += favor.corn;
+    });
+    this.divinity.worldEvents.on('blessing', favor => {
+      this.gold_ += favor.gold;
+      this.corn_ += favor.corn;
+    });
+    this.divinity.worldEvents.on('retribution', retribution => {
+      defense(retribution);
+    });
   }
 
   createSoldier() {
-    if (this.corn_ >= 100 && this.gold_ >= 100) {
-      this.corn_ -= 100;
-      this.gold_ -= 100;
+    if (this.corn_ >= 10 && this.gold_ >= 10) {
+      this.corn_ -= 10;
+      this.gold_ -= 10;
       const s = new Soldier((this.sId_++).toString(), this.timeFactor_);
+      s.init();
       this.soldiers_[s.name] = s;
       return true;
     }
     return false;
+  }
+
+  sendTrader(amount, type) {
+    return new Promise((resolve, reject) => {
+      const g = type === 'gold' ? amount : 0;
+      const c = type === 'corn' ? amount : 0;
+      if (this.corn_ >= 100 + c && this.gold_ >= 100 + g) {
+        this.corn_ -= 100 + c;
+        this.gold_ -= 100 + g;
+        const t = new Trader('trader', amount, type, this.timeFactor_);
+        t.worldEvents.on('trade', r => {
+          if (r.type === 'gold') {
+            this.gold_ += r.amount;
+          } else {
+            this.corn_ += r.amount;
+          }
+          resolve();
+        });
+        t.trade();
+      } else {
+        reject();
+      }
+    });
+  }
+
+  power() {
+    let power = 0;
+    for (let s in this.soldiers_) {
+      power += this.soldiers_[s].isAlive()*2 + this.so;
+    }
+  }
+
+  defense(power) {
+    if(power > )
   }
 
   get corn() {
@@ -49,7 +98,7 @@ class City {
   }
 
   endWorld() {
-    clearInterval(this.gaiaInterval_);
+    this.divinity.endWorld();
   }
 }
 
--- test/city-test.js
+++ test/city-test.js
@@ -12,32 +12,66 @@ describe('city.js', () => {
       c = new City('test', 1);
     });
 
+    it('should have 100 gold/corn', () => {
+      c.corn.should.be.equal(100);
+      c.gold.should.be.equal(100);
+    });
+  });
+
+  describe('Create soldier', () => {
+    let c;
+    beforeEach(() => {
+      c = new City('test', 1);
+    });
+
     it('should create soldiers', () => {
-      c.gold_.should.be.equal(100);
-      c.corn_.should.be.equal(100);
       c.createSoldier().should.be.equal(true);
       c.createSoldier().should.be.equal(true);
-      c.gold_.should.be.equal(80);
-      c.corn_.should.be.equal(80);
+      c.gold.should.be.equal(80);
+      c.corn.should.be.equal(80);
       Object.keys(c.soldiers_).length.should.be.equal(2);
     });
 
     it('shouldn\'t create soldier without enough gold', () => {
       c.gold_ = 0;
-      c.gold_.should.be.equal(0);
-      c.corn_.should.be.equal(100);
       c.createSoldier().should.be.equal(false);
-      c.gold_.should.be.equal(0);
-      c.corn_.should.be.equal(100);
+      c.gold.should.be.equal(0);
+      c.corn.should.be.equal(100);
     });
 
     it('shouldn\'t create soldier without enough corn', () => {
       c.corn_ = 0;
-      c.gold_.should.be.equal(100);
-      c.corn_.should.be.equal(0);
       c.createSoldier().should.be.equal(false);
-      c.gold_.should.be.equal(100);
-      c.corn_.should.be.equal(0);
+      c.gold.should.be.equal(100);
+      c.corn.should.be.equal(0);
+    });
+  });
+
+  describe('Create trader', () => {
+    let c;
+    beforeEach(() => {
+      c = new City('test', 1);
+    });
+
+    it('should send a trader', async () => {
+      c.gold_ = 200;
+      await c.sendTrader(100, 'gold');
+      c.gold.should.be.equal(0);
+      c.corn.should.be.equal(110);
+    });
+
+    it('shouldn\'t create soldier without enough gold', () => {
+      c.gold_ = 0;
+      c.createSoldier().should.be.equal(false);
+      c.gold.should.be.equal(0);
+      c.corn.should.be.equal(100);
+    });
+
+    it('shouldn\'t create soldier without enough corn', () => {
+      c.corn_ = 0;
+      c.createSoldier().should.be.equal(false);
+      c.gold.should.be.equal(100);
+      c.corn.should.be.equal(0);
     });
   });
 });
