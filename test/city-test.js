b1c1a23b597e74399bc54112bc85bffeba4d16ba
Methacrylon <methacrylon@gmail.com>
2018-05-19 17:06:30 +0200

    Update test to verify amount of gold/corn

--- test/city-test.js
+++ test/city-test.js
@@ -13,21 +13,31 @@ describe('city.js', () => {
     });
 
     it('should create soldiers', () => {
-      c.gold_ = 200;
-      c.corn_ = 200;
+      c.gold_.should.be.equal(100);
+      c.corn_.should.be.equal(100);
       c.createSoldier().should.be.equal(true);
       c.createSoldier().should.be.equal(true);
+      c.gold_.should.be.equal(80);
+      c.corn_.should.be.equal(80);
       Object.keys(c.soldiers_).length.should.be.equal(2);
     });
 
     it('shouldn\'t create soldier without enough gold', () => {
       c.gold_ = 0;
+      c.gold_.should.be.equal(0);
+      c.corn_.should.be.equal(100);
       c.createSoldier().should.be.equal(false);
+      c.gold_.should.be.equal(0);
+      c.corn_.should.be.equal(100);
     });
 
     it('shouldn\'t create soldier without enough corn', () => {
       c.corn_ = 0;
+      c.gold_.should.be.equal(100);
+      c.corn_.should.be.equal(0);
       c.createSoldier().should.be.equal(false);
+      c.gold_.should.be.equal(100);
+      c.corn_.should.be.equal(0);
     });
   });
 });
