097065decce47010aaddb6b60626b3ae587ff2d1
Methacrylon <methacrylon@gmail.com>
2018-05-20 22:50:27 +0200

    Main loop

---
+++ game.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+const {Player} = require('./player');
+const {Input} = require('./input');
+
+function play(player, other, action) {
+  switch (action) {
+    case '0':
+      player.createArmy();
+      break;
+    case '1':
+      player.sendTrader('gold');
+      break;
+    case '2':
+      player.sendTrader('corn');
+      break;
+    case '3':
+      player.offer('gold');
+      break;
+    case '4':
+      player.offer('corn');
+      break;
+    case '5':
+      player.attack(other);
+      break;
+    default:
+      break;
+  }
+}
+
+function tour(input, p1, p2) {
+  input.prompt(p1).then(action => {
+    play(p1, p2, action);
+    if (p1.onGame && p2.onGame) {
+      input.prompt(p2).then(action => {
+        play(p2, p1, action);
+        tour(input, p1, p2);
+      });
+    }
+  }).catch(() => {
+    input.prompt(p2).then(action => {
+      play(p2, p1, action);
+      if (p1.onGame && p2.onGame) {
+        tour(input, p1, p2);
+      }
+    }).catch(() => {
+      if (p1.onGame && p2.onGame) {
+        tour(input, p1, p2);
+      }
+    });
+  });
+}
+
+async function game() {
+  const p1 = new Player('p1', 1000);
+  const p2 = new Player('p2', 1000);
+  const input = new Input();
+  p1.worldEvents.on('lost', () => {
+    console.log('Player 2 won!');
+    p1.endWorld();
+    p2.endWorld();
+    input.close();
+    process.exit(0);
+  });
+  p2.worldEvents.on('lost', () => {
+    console.log('Player 1 won!');
+    p1.endWorld();
+    p2.endWorld();
+    input.close();
+    process.exit(0);
+  });
+  p1.init();
+  p2.init();
+  tour(input, p1, p2);
+}
+
+game();
