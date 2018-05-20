31bbfc0edc127a43ab8d9990d6eb42451a7ece3e
Methacrylon <methacrylon@gmail.com>
2018-05-20 17:05:40 +0200

    Add offering, change stats

--- player.js
+++ player.js
@@ -4,38 +4,51 @@ class Player {
   constructor(name, timeFactor) {
     this.name_ = name;
     this.timeFactor_ = timeFactor || 1000;
-    this.city = new City(name, this.timeFactor_);
+    this.city_ = new City(name, this.timeFactor_);
   }
 
   init() {
-    this.city.init();
+    this.city_.init();
   }
 
   createArmy() {
     for (let i = 0; i < 10; i++) {
-      this.city.createSoldier();
+      this.city_.createSoldier();
     }
   }
 
   sendTrader(type) {
-    return this.city.sendTrader(100, type);
+    return this.city_.sendTrader(100, type);
   }
 
   attack(other) {
-    return this.city.attack(other);
+    return this.city_.attack(other.city_);
+  }
+
+  offer(type) {
+    return this.city_.offering(100, type);
   }
 
   stats() {
     return {
-      gold: this.city.gold,
-      corn: this.city.corn,
-      soldiers: {alives: this.city.alives, hurts: this.city.hurts},
-      life: this.city.life_
+      gold: this.city_.gold,
+      corn: this.city_.corn,
+      alives: this.city_.alives,
+      hurts: this.city_.hurts,
+      life: this.city_.life_
     };
   }
 
+  get city() {
+    return this.city_;
+  }
+
+  get name() {
+    return this.name_;
+  }
+
   endWorld() {
-    this.city.endWorld();
+    this.city_.endWorld();
   }
 }
 
--- test/player-test.js
+++ test/player-test.js
@@ -25,6 +25,8 @@ describe('player.js', () => {
       p.city.gold_ = 200;
       p.sendTrader('gold').then(() => {
         p.city.corn_.should.be.equal(110);
+      }).catch(() => {
+        p.city.corn_.should.be.equal(100);
       });
       p.city.corn_.should.be.equal(0);
       p.city.gold_.should.be.equal(0);
@@ -36,6 +38,30 @@ describe('player.js', () => {
       p.stats().soldiers.alives.should.be.equal(0);
       p.stats().soldiers.hurts.should.be.equal(0);
       p.stats().life.should.be.equal(100000);
+    });
+  });
+
+  describe('Attack', () => {
+    let p1;
+    let p2;
+    beforeEach(() => {
+      p1 = new Player('p1', 1);
+      p2 = new Player('p2', 1);
+    });
+
+    afterEach(() => {
+      p1.endWorld();
+      p2.endWorld();
+    });
+
+    it('should attack other', async () => {
+      p1.createArmy();
+      p2.createArmy();
+      await p1.attack(p2);
+      p2.city.alives.should.be.equal(10);
+      p2.city.hurts.should.be.equal(10);
+      p1.city.alives.should.be.below(10);
+      p1.city.hurts.should.be.equal(p1.city.alives);
     });
   });
 });
